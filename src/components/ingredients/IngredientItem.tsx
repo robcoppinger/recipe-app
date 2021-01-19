c0f79cbdab1a840d139242cf772605337837d766
Rob Coppinger <coppinger.robert@gmail.com>
2021-01-19 15:06:43 -0800

    update IngredientItem to use Deleteable component

--- src/components/ingredients/IngredientItem.tsx
+++ src/components/ingredients/IngredientItem.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../../redux';
-import {selectors} from '../../redux/modules/ingredients/Ingredients';
+import {actions, selectors} from '../../redux/modules/ingredients/Ingredients';
 import {Text} from '../common/Text';
+import {Deleteable} from '../common/Deleteable';
+
+const INGREDIENT_ITEM_HEIGHT = 60;
 
 type IngredientItemProps = {
   ingredientId: string;
@@ -14,20 +17,33 @@ export const IngredientItem = ({
   ingredientId,
   recipeId,
 }: IngredientItemProps) => {
+  const dispatch = useDispatch();
   const ingredient = useSelector((st: RootState) =>
     selectors.ingredient(st, ingredientId),
   );
   const {name, amount, unit} = ingredient;
 
+  const deleteItem = () =>
+    dispatch(actions.deleteIngredient(ingredientId, recipeId));
+
   return (
-    <IngredientContainer>
-      <IngredientText>{name}</IngredientText>
-      <AmountText>{`${amount || ''} ${unit || ''}`}</AmountText>
-    </IngredientContainer>
+    <Deleteable
+      containerHeight={INGREDIENT_ITEM_HEIGHT}
+      onDeleteAnimationComplete={deleteItem}>
+      {({interceptPress}) => (
+        <IngredientContainer
+          activeOpacity={1}
+          onPress={() => interceptPress(() => {})}>
+          <IngredientText>{name}</IngredientText>
+          <AmountText>{`${amount || ''} ${unit || ''}`}</AmountText>
+        </IngredientContainer>
+      )}
+    </Deleteable>
   );
 };
 
-const IngredientContainer = styled.View`
+const IngredientContainer = styled.TouchableOpacity`
+  height: 100%;
   flex-direction: row;
   background-color: ${(props) => props.theme.colors.paper};
   padding: ${(props) => props.theme.itemPadding};
