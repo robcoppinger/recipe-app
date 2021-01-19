31599ea0b640ca83d6de753ce8a130dea312f371
Rob Coppinger <coppinger.robert@gmail.com>
2021-01-18 18:01:13 -0800

    make NewShoppingListItem react animate on item deletion

--- src/components/shoppingList/NewShoppingListItem.tsx
+++ src/components/shoppingList/NewShoppingListItem.tsx
@@ -1,19 +1,34 @@
 import React, {useState} from 'react';
+import Animated, {
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import {useDispatch} from 'react-redux';
 import styled from 'styled-components/native';
 import {v4 as uuidV4} from 'uuid';
 import {actions} from '../../redux/modules/shoppingListItems/ShoppingListItems';
 import {TextInput} from '../common/TextInput';
+import {animationConfig, SHOPPING_LIST_ITEM_HEIGHT} from './ShoppingListItem';
 
 type NewShoppingListItemProps = {
   shoppingListId: string;
+  offset: number;
+  deletingId: Animated.SharedValue<string | undefined>;
 };
+
+export const NEW_SHOPPINGLIST_ITEM_HEIGHT = 50;
 
 export const NewShoppingListItem = ({
   shoppingListId,
+  offset,
+  deletingId,
 }: NewShoppingListItemProps) => {
   const dispatch = useDispatch();
   const [newItem, setNewItem] = useState('');
+  const translateY = useSharedValue(offset);
+
   const addItem = () => {
     if (newItem === '') return;
     const newId = uuidV4();
@@ -26,19 +41,46 @@ export const NewShoppingListItem = ({
     );
     setNewItem('');
   };
+
+  useAnimatedReaction(
+    () => deletingId.value,
+    (dId) => {
+      if (dId) {
+        translateY.value = withTiming(
+          translateY.value - SHOPPING_LIST_ITEM_HEIGHT,
+          animationConfig,
+        );
+      }
+    },
+  );
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      height: NEW_SHOPPINGLIST_ITEM_HEIGHT,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      transform: [{translateY: translateY.value}],
+      justifyContent: 'center',
+    };
+  });
+
   return (
-    <NewItem
-      placeholder="new Item"
-      value={newItem}
-      onChangeText={setNewItem}
-      blurOnSubmit={newItem === ''}
-      onSubmitEditing={addItem}
-    />
+    <Animated.View style={containerStyle}>
+      <NewItem
+        placeholder="new Item"
+        value={newItem}
+        onChangeText={setNewItem}
+        blurOnSubmit={newItem === ''}
+        onSubmitEditing={addItem}
+      />
+    </Animated.View>
   );
 };
 
 const NewItem = styled(TextInput)`
-  font-weight: 500;
+  height: 100%;
   border-bottom-width: 1px;
   padding-bottom: 12px;
   padding-top: 12px;
