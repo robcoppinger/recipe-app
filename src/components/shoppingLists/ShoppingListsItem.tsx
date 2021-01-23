924c368a9cfd2218a920a19934edb1d8479d80a7
Rob Coppinger <coppinger.robert@gmail.com>
2021-01-22 22:18:48 -0800

    make shopping list entries deleteable on swipe

--- src/components/shoppingLists/ShoppingListsItem.tsx
+++ src/components/shoppingLists/ShoppingListsItem.tsx
@@ -1,29 +1,52 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import styled from 'styled-components/native';
 import {RootState} from '../../redux';
-import {selectors} from '../../redux/modules/shoppingLists/ShoppingLists';
+import {
+  actions,
+  selectors,
+} from '../../redux/modules/shoppingLists/ShoppingLists';
+import {Deleteable} from '../common/Deleteable';
 import {Text} from '../common/Text';
 
 type ShoppingListsItemProps = {
   shoppingListId: string;
 };
 
+const SHOPPING_LISTS_ITEM_HEIGHT = 60;
+
 export const ShoppingListsItem = ({shoppingListId}: ShoppingListsItemProps) => {
   const {navigate} = useNavigation();
+  const dispatch = useDispatch();
+
   const shoppingList = useSelector((s: RootState) =>
     selectors.shoppingList(s, shoppingListId),
   );
 
+  const deleteShoppingList = () => {
+    dispatch(actions.deleteShoppingList(shoppingListId));
+  };
+
   return (
-    <Container onPress={() => navigate('ShoppingList', {shoppingListId})}>
-      <ItemText>{shoppingList.name}</ItemText>
-    </Container>
+    <Deleteable
+      containerHeight={SHOPPING_LISTS_ITEM_HEIGHT}
+      onDeleteAnimationComplete={deleteShoppingList}>
+      {({interceptPress}) => (
+        <Container
+          onPress={() =>
+            interceptPress(() => navigate('ShoppingList', {shoppingListId}))
+          }>
+          <ItemText>{shoppingList.name}</ItemText>
+        </Container>
+      )}
+    </Deleteable>
   );
 };
 
 const Container = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
   background-color: ${(props) => props.theme.colors.paper}
   padding: ${(props) => props.theme.itemPadding}
   border-color: ${(props) => props.theme.colors.itemSeparator};
