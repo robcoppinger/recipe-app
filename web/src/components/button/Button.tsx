6a0dfd2e108bc2ecef6db2aaf7e5c8cf2b340a6f
Rob Coppinger <coppinger.robert@gmail.com>
2021-03-21 14:45:50 -0700

    create button component

---
+++ web/src/components/button/Button.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import {ButtonHTMLAttributes, forwardRef} from 'react';
+import styles from './Button.module.scss';
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: any;
+  variant?: 'filled' | 'outline';
+}
+
+export const Button = forwardRef<HTMLButtonElement, IButton>(
+  ({children, className, variant, ...buttonProps}, ref) => {
+    return (
+      <button
+        type="button"
+        {...buttonProps}
+        className={classNames(styles.button, className && className, {
+          [styles.filled]: variant === 'filled',
+          [styles.outline]: variant === 'outline',
+          [styles.disabled]: buttonProps.disabled,
+        })}>
+        {children}
+      </button>
+    );
+  },
+);
