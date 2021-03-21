f98b739c89ca110f837bbd35d4c3ce3068d8b96a
Rob Coppinger <coppinger.robert@gmail.com>
2021-03-21 12:52:57 -0700

    create TextInput component

---
+++ web/src/components/textInput/TextInput.tsx
@@ -0,0 +1,29 @@
+import {forwardRef, InputHTMLAttributes} from 'react';
+import styles from './TextInput.module.scss';
+
+interface ITextInput extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  id: string;
+  required?: boolean;
+}
+
+export const TextInput = forwardRef<HTMLInputElement, ITextInput>(
+  ({id, label, required = false, ...inputProps}: ITextInput, ref) => (
+    <>
+      {label && (
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        name={id}
+        type="text"
+        className={styles.input}
+        aria-label={label || inputProps.placeholder}
+        aria-required={required}
+        {...inputProps}
+      />
+    </>
+  ),
+);
