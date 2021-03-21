8c84dfffb366ebee66bcea2a2bc23cc81ce8107d
Rob Coppinger <coppinger.robert@gmail.com>
2021-03-20 21:06:10 -0700

    add react-router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,25 @@
 import './css/base.css';
 import './css/themes.css';
+import {Home} from './screens/home';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {Register} from './screens/register';
+import {Login} from './screens/login';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
