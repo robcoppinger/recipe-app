4c671f1007f4da43050bd91741af2206b2fb5f01
Rob Coppinger <coppinger.robert@gmail.com>
2021-03-27 18:45:31 -0700

    create auth middleware

---
+++ api/src/middleware/AuthenticationMiddleware.ts
@@ -0,0 +1,49 @@
+import { Middleware, ExpressMiddlewareInterface } from 'routing-controllers'
+import jwt from 'jsonwebtoken'
+import { env } from '../env'
+import { getRepository } from 'typeorm'
+import { User } from 'entities/User'
+
+function extractTokenFromRequest(request: any) {
+  const token = request.headers.authorization
+
+  if (!token) {
+    return null
+  }
+
+  if (token.startsWith('Bearer ')) {
+    // remove Bearer from token
+    return token.slice(7, token.length)
+  }
+
+  return token
+}
+
+@Middleware({ type: 'before' })
+export class AuthenticationMiddleware implements ExpressMiddlewareInterface {
+  use(request: any, response: any, next: (err: any) => any): void {
+    const token = extractTokenFromRequest(request)
+
+    if (!token) {
+      // no token to decode
+      return next(null)
+    }
+
+    jwt.verify(token, env.app.secret, (err: any, decoded: any) => {
+      if (err) {
+        return next(err)
+      }
+
+      const userRepo = getRepository(User)
+      userRepo
+        .findOne({ where: { id: decoded.id } })
+        .then((user) => {
+          request.user = user
+          next(null)
+        })
+        .catch((e) => {
+          next(e)
+        })
+    })
+  }
+}
