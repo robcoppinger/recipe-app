f21fa7570edf77567779608efc1360152ac6cfd1
Rob Coppinger <coppinger.robert@gmail.com>
2021-04-25 20:21:53 -0700

    update app navigator to use authenticated stacks

--- mobile/src/navigators/AppNavigator.tsx
+++ mobile/src/navigators/AppNavigator.tsx
@@ -1,60 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { useTheme } from '../context/ThemeContext';
-import { DrawerContent } from './DrawerContent';
-import { SvgImage } from '../components/common/SvgImage';
-import { RecipeNavigator } from './RecipeNavigator';
-import { ShoppingListNavigator } from './ShoppingListNavigator';
-import { RoadmapNavigator } from './RoadmapNavigator';
 import { UnauthenticatedNavigator } from './UnauthenticatedNavigator';
-
-const Drawer = createDrawerNavigator();
+import { AuthenticatedNavigator } from './AuthenticatedNavigator';
+import { useSelector } from 'react-redux';
+import { selectors } from '../redux/modules/auth/Auth';
+import { env } from '../../env';
 
 export const AppNavigator = () => {
-  const theme = useTheme();
+  const isLoggedIn = useSelector(selectors.isLoggedIn);
+  const isAuthenticated = isLoggedIn || !env.featureFlags.isAuthEnabled;
 
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        drawerContentOptions={{
-          activeTintColor: theme.colors.primary,
-          inactiveTintColor: theme.colors.text,
-          labelStyle: {
-            fontFamily: theme.defaultFontFamily.regular,
-            fontWeight: '600',
-          },
-        }}
-        drawerContent={(props) => <DrawerContent {...props} />}
-        initialRouteName="Recipes">
-        <Drawer.Screen
-          name="Recipes"
-          component={RecipeNavigator}
-          options={{
-            drawerIcon: ({ color }) => (
-              <SvgImage icon="list" fill={color} size={30} />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="Shopping List"
-          component={ShoppingListNavigator}
-          options={{
-            drawerIcon: ({ color }) => (
-              <SvgImage icon="cart" fill={color} size={30} />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="Roadmap"
-          component={RoadmapNavigator}
-          options={{
-            drawerIcon: ({ color }) => (
-              <SvgImage icon="info" fill={color} size={30} />
-            ),
-          }}
-        />
-      </Drawer.Navigator>
+      {isAuthenticated ? (
+        <AuthenticatedNavigator />
+      ) : (
+        <UnauthenticatedNavigator />
+      )}
     </NavigationContainer>
   );
 };
